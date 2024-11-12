64bea86acddfba0d40b6c312fdef0cc4ab2efff0
Simpag <simon.granstrom@gmail.com>
2024-11-13 00:13:33 +0100

    fix: load with or without bias

--- util/model_util.py
+++ util/model_util.py
@@ -11,7 +11,7 @@ def load_model(path) -> DCGAN:
     checkpoint = load_checkpoint(path)
 
     architecture = checkpoint['config']['model']['architecture']
-    model = DCGAN(architecture=architecture)
+    model = DCGAN(architecture=architecture, use_bias=checkpoint['config']['model'].get('use_bias', False))
 
     model.load_state_dict(checkpoint['model_state_dict'])
     return model
