3b6122455e0504bbbbe8577b7642fc8be246b371
Simpag <simon.granstrom@gmail.com>
2024-11-13 17:12:15 +0100

    fix: image plot for tensors

--- util/plot_images.py
+++ util/plot_images.py
@@ -26,15 +26,15 @@ def plot_images(original: list, generated: list, grayscale=True):
 
             if row == 0:
                 if grayscale:
-                    axs[0, col+1].imshow(original[col][0, 0], cmap='gray')
+                    axs[0, col+1].imshow(original[col, 0], cmap='gray')
                 else:
-                    axs[0, col+1].imshow(original[col][0].transpose((1,2,0))) # imshow expects [width, height, channels]
+                    axs[0, col+1].imshow(original[col, 0].transpose((1,2,0))) # imshow expects [width, height, channels]
                 axs[0, col+1].axis('off')
             else:
                 if grayscale:
-                    axs[row, col+1].imshow(generated[col][row-1][0, 0], cmap='gray')
+                    axs[row, col+1].imshow(generated[col, row-1, 0], cmap='gray')
                 else:
-                    axs[row, col+1].imshow(generated[col][row-1][0].transpose((1,2,0)))
+                    axs[row, col+1].imshow(generated[col, row-1].transpose((1,2,0)))
                 axs[row, col+1].axis('off')
 
     plt.subplots_adjust(wspace=0, hspace=0)
